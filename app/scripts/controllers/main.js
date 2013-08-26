8ee76688c8d7de55ed409502099cb3462bd8a43f
gdi2290 <github@gdi2290.com>
2013-08-25 22:34:48 -0700

    move search bar in main view

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,8 +5,6 @@ hackreactor
     function($scope, $location, $routeParams) {
       $scope.directory = window._directoryJSON;
 
-      $scope.searchText = '';
-
       $scope.personUrl = function(person) {
         var index = ($routeParams.id) ? $routeParams.id : $scope.directory.indexOf(person);
         return '#/person/' + index;
