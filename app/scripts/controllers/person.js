02e2a2d647ea6b405d3ccdb3c5b894f291baadbf
gdi2290 <github@gdi2290.com>
2013-08-25 18:50:10 -0700

    add person view and logic

--- app/scripts/controllers/person.js
+++ app/scripts/controllers/person.js
@@ -3,5 +3,15 @@
 angular.module('hackreactorApp')
   .controller('PersonCtrl', ['$scope', '$routeParams', '$location',
     function($scope, $routeParams, $location) {
+      $scope.person = $scope.directory[$routeParams.id];
+      $scope.save = function(person) {
+        console.log('save' + person);
+        $location.path('/');
+      };
+      $scope.remove = function(person) {
+        var index = $scope.directory.indexOf(person);
+        $scope.directory.splice(index, 1);
+        $location.path('/');
+      };
     }
   ]);
