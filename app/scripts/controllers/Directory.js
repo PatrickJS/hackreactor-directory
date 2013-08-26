44181689f36864e2d98c8184c9aa971108c70699
gdi2290 <github@gdi2290.com>
2013-08-25 18:17:04 -0700

    add delete button

--- app/scripts/controllers/Directory.js
+++ app/scripts/controllers/Directory.js
@@ -1,10 +1,13 @@
 'use strict';
 
 angular.module('hackreactorApp')
-  .controller('DirectoryCtrl', ['$scope', 'Directory', '$window',
-    function($scope, Directory, $window) {
+  .controller('DirectoryCtrl', ['$scope', 'Directory',
+    function($scope, Directory) {
       $scope.directory = window._directoryJSON;
-
+      $scope.remove = function(person) {
+        var index = $scope.directory.indexOf(person);
+        $scope.directory.splice(index, 1);
+      };
     } // end function
   ]);
 
