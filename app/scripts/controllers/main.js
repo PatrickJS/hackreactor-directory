01876a0f6a8919e72e2bbea1ba2ef0f0c0b2c4c7
gdi2290 <github@gdi2290.com>
2013-08-25 18:50:31 -0700

    move directory and personUrl logic to mail controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,14 @@
 'use strict';
 
 angular.module('hackreactorApp')
-  .controller('MainCtrl', ['$scope', function($scope) {
+  .controller('MainCtrl', ['$scope',
+    function($scope) {
+      $scope.directory = window._directoryJSON;
 
-  }]);
+      $scope.personUrl = function(person) {
+        var index = $scope.directory.indexOf(person);
+        return '#/person/' + index;
+      };
+
+    }
+  ]);
