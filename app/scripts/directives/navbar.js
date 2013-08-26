3b4f2b3163541f5b658bcfcbde409da8e7700cbf
gdi2290 <github@gdi2290.com>
2013-08-25 18:50:50 -0700

    remove reload from navbar

--- app/scripts/directives/navbar.js
+++ app/scripts/directives/navbar.js
@@ -8,7 +8,7 @@ angular.module('hackreactorApp')
       },
       template: '<div class="navbar navbar-inverse navbar-fixed-top">'+
                     '<div class="navbar-inner">'+
-                      '<a class="brand" href="#" target="_blank">'+
+                      '<a class="brand" href="/">'+
                         '<img src="http://hackreactor.com/wp-content/uploads/2013/04/180px.png">'+
                       '</a>'+
                       '<ul class="nav pull-right">'+
